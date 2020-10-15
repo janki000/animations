42bb706a88cdac442cd28297c4d98332ea21b4bc
janki000 <54926536+janki000@users.noreply.github.com>
2020-10-15 13:20:05 -0400

    animation_modification

--- script.js
+++ script.js
@@ -3,173 +3,9 @@ repeat:-2,
 yoyo:true
 })
 
-.to('.f1', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f2', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f3', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f4', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f5', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f6', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f7', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f8', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f9', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f10', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f11', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f12', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f13', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f14', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f15', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f16', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f17', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f18', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f19', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f20', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f21', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f22', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f23', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f24', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f25', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f26', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f27', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f28', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f29', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f30', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f31', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f32', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f33', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f34', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f35', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f36', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f37', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f38', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f39', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f40', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f41', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f42', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f43', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f44', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f45', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f46', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f47', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f48', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f49', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f50', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f51', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f52', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f53', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f54', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f55', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f56', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f57', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f58', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f59', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f60', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f61', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f62', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f63', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f64', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f65', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f66', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f67', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f68', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f69', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f70', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f71', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f72', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f73', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f74', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f75', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f76', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f77', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f78', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f79', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f80', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f81', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f82', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f83', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f84', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f85', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f86', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f87', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f88', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f89', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f90', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f91', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f92', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f93', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f94', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f95', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f96', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f97', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f98', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f99', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f100', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f101', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f102', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f103', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f104', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f105', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f106', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f107', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f108', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f109', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f110', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f111', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f112', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f113', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f114', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f115', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f116', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f117', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f118', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f119', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f120', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f121', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f122', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f123', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f124', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f125', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f126', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f127', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f128', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f129', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f130', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f131', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f132', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f133', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f134', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f135', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f136', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f137', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f138', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f139', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f140', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f141', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f142', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f143', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f144', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f145', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f146', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f147', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f148', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f149', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f150', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f151', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f152', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f153', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f154', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f155', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f156', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f157', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f158', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f159', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f160', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f161', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f162', {opacity: 0, duration: 0.2, delay: '25%'})
-.to('.f163', {opacity: 0, duration: 0.2, delay: '25%'})
+.to('.f', {stagger: 0.2, opacity: 0, duration: 0.2, delay: '25%'})
 
-.to('.tronc', {opacity: 0, duration: 0.2, delay: '35%'})
-
-
+.to('.tronc', {opacity: 2, duration: 0.2, delay: '35%'})
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -206,6 +42,7 @@ const t2 = gsap.timeline({defaults: {ease: 'linear'},
     x: '-1100px',
     opacity: -5,
     delay: 0.5,
+    stagger: 0.2,
     scrollTrigger: {
       scrub: 0.3,
       scale: 2,
@@ -216,11 +53,13 @@ const t2 = gsap.timeline({defaults: {ease: 'linear'},
     }
   })
   
+  
   .to('.oiseau2', { 
     y: '2100px',
     x: '-1100px',
     opacity: -5,
     delay: 0.5,
+    stagger: 0.2,
     scrollTrigger: {
       scrub: 0.3,
       scale: 2,
@@ -235,6 +74,7 @@ const t2 = gsap.timeline({defaults: {ease: 'linear'},
     x: '2100px',
     opacity: -5,
     delay: 0.5,
+    stagger: 0.2,
     scrollTrigger: {
       scrub: 0.3,
       scale: 2,
@@ -249,6 +89,7 @@ const t2 = gsap.timeline({defaults: {ease: 'linear'},
     x: '2100px',
     opacity: -5,
     delay: 0.5,
+    stagger: 0.2,
     scrollTrigger: {
       scrub: 0.3,
       scale: 2,
@@ -262,6 +103,7 @@ const t2 = gsap.timeline({defaults: {ease: 'linear'},
     x: '2100px',
     opacity: -5,
     delay: 0.5,
+    stagger: 0.2,
     scrollTrigger: {
       scrub: 0.3,
       scale: 2,
@@ -269,9 +111,11 @@ const t2 = gsap.timeline({defaults: {ease: 'linear'},
       end: 'bottom 90%',
     }
   })
+  
 
   const t3 = gsap.timeline({defaults: {ease: 'linear'},
 })
+
 .to('.nature', { 
   y: '2100px',
   rotation: 180,
@@ -380,8 +224,3 @@ const t2 = gsap.timeline({defaults: {ease: 'linear'},
     end: 'bottom 90%',
   }
 })
-
-
-
-  
-
